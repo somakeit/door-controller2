c43fab6ff75db9af7a2d74f57e0c7256cb96a175
Bracken Dawson <abdawson@gmail.com>
2015-04-14 22:52:44 +0100

    Add crc16
    now ready for a MIFARE Classic sector

--- door-controller.py
+++ door-controller.py
@@ -1,4 +1,4 @@
-import bcrypt, sys
+import bcrypt, sys, crc16
 
 BCRYPT_BASE64_DICT = './ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 BCRYPT_VERSION = ['2', '2a', '2y']
@@ -26,10 +26,23 @@ cardable.append(cost);
 for i in range(40):
     cardable.append(int(((binary >> (i * 8)) & 0xff)))
 
+csum = crc16.crc16xmodem(hashed)
+print csum
+
+cardable.append(csum >> 8)
+cardable.append(csum & 255)
+cardable.append(0) #reserved bytes
+cardable.append(0)
+
 print cardable
+#cardable[10] = cardable[10] + 1 #bit error
+#print cardable
 
 #################################################################
 
+csum2 = (cardable.pop(44) << 8) + cardable.pop(44)
+reserved = cardable.pop(len(cardable)-1)
+reserved = cardable.pop(len(cardable)-1)
 version2 = BCRYPT_VERSION[cardable.pop(0)]
 reserved = cardable.pop(0)
 reserved = cardable.pop(0)
@@ -41,13 +54,16 @@ for c in cardable:
     binary2 = binary2 + (c << i)
     i += 8
 
-hashed2 = ''
+saltdigest2 = ''
 for i in range(53):
-    hashed2 = hashed2 + str(list(BCRYPT_BASE64_DICT)[(binary2 >> (i * 6)) & 63])
+    saltdigest2 = saltdigest2 + str(list(BCRYPT_BASE64_DICT)[(binary2 >> (i * 6)) & 63])
 
-sys.stdout.write('$')
-sys.stdout.write(str(version2))
-sys.stdout.write('$')
-sys.stdout.write(str(cost2))
-sys.stdout.write('$')
+hashed2 = '$' + str(version2) + '$' + str(cost2) + '$' + saltdigest2
 print hashed2
+
+csum3 = crc16.crc16xmodem(hashed2)
+print csum3
+if (csum2 == csum3):
+    print 'valid'
+else:
+    print 'INVALID'
