df797858c7eee9449ff1cf4a22f6622b6e4f1aaf
Bracken Dawson <abdawson@gmail.com>
2015-07-11 20:15:54 +0000

    more

--- doord.py
+++ doord.py
@@ -39,27 +39,53 @@ class DoorService:
 class Tag:
     uid = None
     nfc = None
+    db = None
+    count = None
+    count_a = None
+    count_b = None
 
-    def __init__(self, uid, nfc):
+    def __init__(self, uid, nfc, db):
         self.uid = uid
 	self.nfc = nfc
+        self.db = db
 
     def authenticate(self):
-        #placeholder
-	self.nfc.MFRC522_SelectTag(self.uid)
-	status = self.nfc.Auth_Sector(self.nfc.PICC_AUTHENT1A, 0, [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF], self.uid)
-	if status != self.nfc.MI_OK:
-	    print "Authentication error"
-	    return False
-	(status, data) = self.nfc.Read_Sector(sector)
-	if status == self.nfc.MI_OK:
-	    print data
-	else:
-	    print "Read error"
-	    return False
+        userid = self.db.get_tag_user(self.str_x_uid())
+        if userid == None:
+            print "Tag " + self.str_x_uid() + " is alien"
+            return False
 
-        nfc.MFRC522_StopCrypto1() #TODO work out the correct time to run this.
-	return True
+        if self.db.is_tag_blacklisted(self.str_x_uid()):
+            print "BLACKLISTED TAG DETECTED: " + self.str_x_uid
+
+        username = self.db.get_user_name(userid)
+        print "Tag is assigned to user " + userid + " (" + username + ")"
+        
+        self.count = self.db.get_tag_count(self.str_x_uid())
+
+        self.count_a = self.validate_sector(self.read_sector(self.db.get_tag_sector_a_sector(self.str_x_uid()), self.db.get_tag_sector_a_key_b(self.str_x_uid()), self.nfc.PICC_AUTHENT1B), self.db.get_tag_sector_a_secret(self.str_x_uid()))
+        self.count_b = self.validate_sector(self.read_sector(self.db.get_tag_sector_b_sector(self.str_x_uid()), self.db.get_tag_sector_b_key_b(self.str_x_uid()), self.nfc.PICC_AUTHENT1B), self.db.get_tag_sector_b_secret(self.str_x_uid()))
+
+        #self.nfc.MFRC522_SelectTag(self.uid)
+        #status = self.nfc.Auth_Sector(self.nfc.PICC_AUTHENT1A, 0, [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF], self.uid)
+	#if status != self.nfc.MI_OK:
+        #    print "Authentication error"
+        #    return False
+        #(status, data) = self.nfc.Read_Sector(sector)
+        #if status == self.nfc.MI_OK:
+        #    print data
+        #else:
+        #    print "Read error"
+        #    return False
+        #
+        #nfc.MFRC522_StopCrypto1() #TODO work out the correct time to run this.
+        #return True
+
+    def validate_sector(self, sector_data, secret):
+        #do the things
+        return count
+
+    def read_sector(self, sector, key, keyspec):
         
     def str_x_uid(self):
         return format(self.uid[0], "x") + format(self.uid[1], "x") + format(self.uid[2], "x") + format(self.uid[3], "x")
@@ -86,7 +112,49 @@ class EntryDatabase:
         # pull server copy down, if this initial load fails we will exit and let systemd respawn us
         response = requests.get(self.server_url, {'api_key': self.api_key})
         self.local = json.loads(response.text)
-        print self.local
+
+    def get_tag_user(self, uid):
+        if self.local['tags'].has_key(uid):
+            return self.local['tags'][uid]['assigned_user']
+        else:
+            return None
+
+    def is_tag_blacklisted(self, uid):
+        if self.local['tags'][uid]['blacklisted']:
+            return True
+        else:
+            return False
+
+    def get_tag_count(self, uid):
+        return self.local['tags'][uid]['count']
+
+    def get_tag_sector_a_sector(self, uid):
+        return self.local['tag'][uid]['sector_a_sector']
+
+    def get_tag_sector_b_sector(self, uid):
+        return self.local['tag'][uid]['sector_b_sector']
+
+    def get_tag_sector_a_key_a(self, uid):
+        return self.local['tag'][uid]['sector_a_key_a']
+    
+    def get_tag_sector_a_key_b(self, uid):
+        return self.local['tag'][uid]['sector_a_key_b']
+    
+    def get_tag_sector_b_key_a(self, uid):
+        return self.local['tag'][uid]['sector_b_key_a']
+    
+    def get_tag_sector_b_key_b(self, uid):
+        return self.local['tag'][uid]['sector_b_key_b']
+    
+    def get_tag_sector_a_secret(self, uid):
+        return self.local['tag'][uid]['sector_a_secret']
+
+    def get_tag_sector_b_secter(self, uid):
+        return self.local['tag'][uid]['sector_b_secret']
+
+    def get_user_name(self, userid):
+        return self.local['users'][userid]['name']
+
 
 inst = DoorService()
 inst.main()
